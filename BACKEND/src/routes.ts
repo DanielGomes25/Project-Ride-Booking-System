fa3716588ed620b866679c15238b83f2ab08da3c
Daniel <daniel.gomes.9@hotmail.com>
2024-11-22 18:07:28 -0300

    criando e testando primeira rota

--- BACKEND/src/routes.ts
+++ BACKEND/src/routes.ts
@@ -1,1 +1,8 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+
+
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    fastify.get("/rides/confirm", async (request: FastifyRequest, reply: FastifyReply) => {
+    return { hello: "world" };
+  });
+}
--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,10 +1,15 @@
 import Fastify from "fastify";
-// import cors from "@fastify/cors";
+import { routes } from "./routes";
+import cors from "@fastify/cors";
 
 
 const app = Fastify({ logger: true });
 
 const start = async () => {
+
+  await app.register(routes);
+  await app.register(cors);
+
   try {
     await app.listen({ port: 8080 });
     } catch (err) {
