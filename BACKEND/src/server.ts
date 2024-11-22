e8f40aa72df5e91d093fc6a5b791040abf42eb5b
Daniel <daniel.gomes.9@hotmail.com>
2024-11-22 16:46:08 -0300

    primeiras configs do servidor e adiciona scripts

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,2 +1,15 @@
 import Fastify from "fastify";
-import cors from "@fastify/cors";
+// import cors from "@fastify/cors";
+
+
+const app = Fastify({ logger: true });
+
+const start = async () => {
+  try {
+    await app.listen({ port: 8080 });
+    } catch (err) {
+      process.exit(1);
+    }
+  }
+
+start();
