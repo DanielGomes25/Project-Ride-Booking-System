f022ebe4402f2e884b7d27e935a97426ee8aa13f
Daniel <daniel.gomes.9@hotmail.com>
2024-11-25 19:35:09 -0300

    refatora schema do prisma e adiciona seed

--- BACKEND/src/routes/routes.ts
+++ BACKEND/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from 'fastify';
 
 export async function routes(fastify: FastifyInstance, _options: FastifyPluginOptions) {
-  fastify.get('/ride/estimate', async (_request: FastifyRequest, _reply: FastifyReply) => ({ hello: 'world' }));
+  fastify.get('/teste', async (_request: FastifyRequest, _reply: FastifyReply) => ({ hello: 'world' }));
 }
