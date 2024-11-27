7b025dea3b784745139de6df680ab44a9e4088d8
Daniel <daniel.gomes.9@hotmail.com>
2024-11-27 14:04:24 -0300

    adiciona formulario e redericionamento para as opçoes de viagem

---
+++ FRONTEND/FrontEnd/src/pages/TravelRequest.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/TravelRequest.css';
+import api from '../services/api';
+
+export default function TravelRequest() {
+  const [customerId, setCustomerId] = useState('');
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  
+  
+  const navigate = useNavigate();
+
+  const handleEstimate = async () => {
+    if (customerId === '' || origin === '' || destination === '') {
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    try {
+
+      const response = await api.post('/ride/estimate', {
+        customer_id: customerId,
+        origin,
+        destination,
+      });
+
+
+      navigate('/options', { state: response.data });
+      console.log(response.data);
+
+    } catch (error: unknown) {
+    
+      if (error instanceof Error) {
+        alert('Erro ao calcular estimativa: ' + (error.response?.data?.message || error.message));
+      } else {
+        alert('Erro desconhecido');
+      }
+    }
+  };
+
+  return (
+    <div className="travel-request">
+      <h1>Solicitação de Viagem</h1>
+      <form>
+        <label>
+          ID do Cliente:
+          <input
+            type="text"
+            value={customerId}
+            onChange={(e) => setCustomerId(e.target.value)}
+          />
+        </label>
+        <label>
+          Origem:
+          <input
+            type="text"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          />
+        </label>
+        <label>
+          Destino:
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleEstimate}>
+          Calcular Estimativa
+        </button>
+      </form>
+    </div>
+  );
+}
